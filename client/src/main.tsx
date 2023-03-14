ba9ce195847bea58d931e505706b66e2641f4cf2
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2023-03-15 01:30:42 +0300

    Use best practice vite and eslint configs

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root'),
+);
