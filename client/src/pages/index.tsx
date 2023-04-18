0a0e89b79821f66f20ec92ceb0b4a7912ca6a0a0
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2023-04-18 20:27:43 +0300

    Fix direction

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,31 +1,48 @@
-import { Box, Container, Stack, Typography } from '@mui/material';
-import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { Menu, MenuItem, Sidebar, SubMenu } from 'react-pro-sidebar';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4, textAlign: 'center' }}>
-        {/* <img
+    <Stack flexDirection="row">
+      <Sidebar>
+        <Menu>
+          <SubMenu label="Charts">
+            <MenuItem> Pie charts </MenuItem>
+            <MenuItem> Line charts </MenuItem>
+          </SubMenu>
+          <MenuItem> Documentation </MenuItem>
+          <MenuItem> Calendar </MenuItem>
+        </Menu>
+      </Sidebar>
+      <Stack
+        p="20px 20px"
+        sx={{
+          border: '2px solid red',
+        }}
+      >
+        <Box sx={{ my: 4, textAlign: 'center' }}>
+          {/* <img
           src="public/icons/logo.svg"
           alt="logo"
           style={{
             width: '400px',
           }}
         /> */}
-        <Typography variant="h4" component="h1" gutterBottom>
-          CampusClubs'a hoş geldiniz
-        </Typography>
-        <Typography variant="body1">
-          CampusClubs, üniversite öğrencilerinin birbirleriyle etkileşim
-          kurmalarını sağlayan bir platformdur.
-        </Typography>
-        <Stack gap="20px">
-          <Link to="signup">Kayit ol</Link>
-          <Link to="signin">Giriş yap</Link>
-        </Stack>
-      </Box>
-    </Container>
+          <Typography variant="h4" component="h1" gutterBottom>
+            CampusClubs'a hoş geldiniz
+          </Typography>
+          <Typography variant="body1">
+            CampusClubs, üniversite öğrencilerinin birbirleriyle etkileşim
+            kurmalarını sağlayan bir platformdur.
+          </Typography>
+          <Stack gap="20px">
+            <Link to="signup">Kayit ol</Link>
+            <Link to="signin">Giriş yap</Link>
+          </Stack>
+        </Box>
+      </Stack>
+    </Stack>
   );
 };
 
