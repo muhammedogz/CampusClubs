173449080a5d63f0b21e9f668b64f33e947d53ae
Suleyman <silo1881@gmail.com>
2023-04-29 16:57:25 +0300

    User commands added

--- server/Controllers/UserController.cs
+++ server/Controllers/UserController.cs
@@ -18,91 +18,17 @@ public class UserController : ControllerBase
         _db = db;
     }
 
-    // public IActionResult Index(){
-    //     // var objCategoryList = _db.Categories.ToList();
-    //     IEnumerable<User> objCategoryList = _db.Users;
-    //     return objCategoryList;  
-    // }
-
-    // // GET - CREATE
-    // public IActionResult Create(){
-    //     return View();
-    // }
-
-    // // POST - CREATE
-    // [HttpPost]
-    // [ValidateAntiForgeryToken]
-    // public IActionResult Create(Category objCategory){
-    //     if(objCategory.Name == objCategory.DisplayOrder.ToString()){
-    //         ModelState.AddModelError("name", "Name and Display Order cannot be the same");
-    //     }
-    //     if(ModelState.IsValid){
-    //         _db.Categories.Add(objCategory);
-    //         _db.SaveChanges();
-    //         return RedirectToAction("Index");
-    //     }
-    //     return View(objCategory);
-    // }
-
-    // // GET - CREATE
-    // public IActionResult Edit(int? id){
-    //     if(id == null){
-    //         return NotFound();
-    //     }
-    //     var categoryFromDb = _db.Categories.Find(id);
-    //     // var categoryFromDbFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
-    //     // var categoryFromDbSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
-    //     if(categoryFromDb == null){
-    //         return NotFound();
-    //     }
-
-    //     return View(categoryFromDb);
-    // }
-
-    // // POST - CREATE
-    // [HttpPost]
-    // [ValidateAntiForgeryToken]
-    // public IActionResult Edit(Category objCategory){
-    //     if(objCategory.Name == objCategory.DisplayOrder.ToString()){
-    //         ModelState.AddModelError("name", "Name and Display Order cannot be the same");
-    //     }
-    //     if(ModelState.IsValid){
-    //         _db.Categories.Update(objCategory); // it will find primary key
-    //         _db.SaveChanges();
-    //         return RedirectToAction("Index");
-    //     }
-    //     return View(objCategory);
-    // }
-
-    //     // GET - CREATE
-    // public IActionResult Delete(int? id){
-    //     if(id == null){
-    //         return NotFound();
-    //     }
-    //     var categoryFromDb = _db.Categories.Find(id); //same with Edit
-    //     // var categoryFromDbFirst = _db.Categories.FirstOrDefault(c => c.Id == id);
-    //     // var categoryFromDbSingle = _db.Categories.SingleOrDefault(c => c.Id == id);
-    //     if(categoryFromDb == null){
-    //         return NotFound();
-    //     }
-
-    //     return View(categoryFromDb);
-    // }
-
-    // // POST - CREATE
-    // [HttpPost, ActionName("Delete")]
-    // [ValidateAntiForgeryToken]
-    // public IActionResult DeletePOST(int? id){
-    //     var obj = _db.Categories.Find(id);
-    //     if(obj == null){
-    //         return NotFound();
-    //     }
-    //     _db.Categories.Remove(obj); // it will find primary key
-    //     _db.SaveChanges(); 
-    //     TempData["Success"] = "Category Deleted Successfully";
-    //     return RedirectToAction("Index");
-    // }
-
+    [HttpGet]
+    public IActionResult GetAll()
+    {   // Return all user info
+        try{
+            IEnumerable<User> objCategoryList = _db.Users;
+            return Ok(objCategoryList);
+        }
+        catch (Exception e){
+            return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+        }
+    }
 
     [HttpGet("id")]
     public string GetUserName(int id)
@@ -126,44 +52,90 @@ public class UserController : ControllerBase
         return "User not found";
     }
 
-    // [HttpGet("id")]  // hata veriyor cunku get id
-    // public string EditUserName(int id)
-    // {
-    //     // Get the underlying SqlConnection object
-    //     SqlConnection sqlConnection = (SqlConnection)_db.Database.GetDbConnection();
-
-    //     // get sql connection from appsettings.json
-    //     var sql = "UPDATE * FROM Users WHERE userId = @Id";
-    //     var cmd = new SqlCommand(sql, sqlConnection);
-    //     cmd.Parameters.AddWithValue("@Id", id);
-
-    //     sqlConnection.Open();
-    //     var reader = cmd.ExecuteReader();
-    //     if (reader.Read())
-    //     {
-    //         var name = reader.GetString(1);
-    //         return name;
-    //     }
-
-    //     return "User not found";
-    // }
-
-    
-    // POST - CREATE
     [HttpPost]
-    [ValidateAntiForgeryToken]
-    public string Create(User objCategory){
-        if(objCategory.Name == objCategory.Username.ToString()){
-            ModelState.AddModelError("name", "Name and Display Order cannot be the same");
+    public IActionResult Create(User objCategory)
+    {
+        // Get the underlying SqlConnection object
+        SqlConnection sqlConnection = (SqlConnection)_db.Database.GetDbConnection();
+
+        var sql = "INSERT INTO Users (userId, username, name, email, password, createdDate, deletedDate) VALUES (@Id, @Username, @Name, @Email, @Password, @CreatedDate, @DeletedDate)";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", objCategory.UserId);
+        cmd.Parameters.AddWithValue("@Username", objCategory.Username);
+        cmd.Parameters.AddWithValue("@Name", objCategory.Name);
+        cmd.Parameters.AddWithValue("@Email", objCategory.Email);
+        cmd.Parameters.AddWithValue("@Password", objCategory.Password);
+        cmd.Parameters.AddWithValue("@CreatedDate", objCategory.CreatedDate ?? (object)DBNull.Value);
+        cmd.Parameters.AddWithValue("@DeletedDate", objCategory.DeletedDate ?? (object)DBNull.Value);
+        /* ?? (object)DBNull.Value makes allow nulls by converting DBNull */
+
+        sqlConnection.Open();
+        int rowsAffected = cmd.ExecuteNonQuery();
+        sqlConnection.Close();
+
+        if (rowsAffected > 0)
+        {
+            return Ok("User created successfully.");
         }
-        if(ModelState.IsValid){
-            _db.Users.Add(objCategory);
-            _db.SaveChanges();
-            // return RedirectToAction("Index");
-            return "Successfully created";
+        else
+        {
+            return BadRequest("BadRequest: Unable to create the user.");
         }
-        // return View(objCategory);
-        return "Error occured when creating";;
     }
+
+    [HttpDelete("{userId}")]
+    public IActionResult Delete(int userId)
+    {
+        // Get the underlying SqlConnection object
+        SqlConnection sqlConnection = (SqlConnection)_db.Database.GetDbConnection();
+
+        var sql = "DELETE FROM Users WHERE userId = @Id";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", userId);
+
+        sqlConnection.Open();
+        int rowsAffected = cmd.ExecuteNonQuery();
+        sqlConnection.Close();
+
+        if (rowsAffected > 0)
+        {
+            return Ok("User deleted successfully.");
+        }
+        else
+        {
+            return NotFound("User not found.");
+        }
+    }
+
+    [HttpPut("{userId}")]
+    public IActionResult Update(int userId, User updatedUser)
+    {
+        // Get the underlying SqlConnection object
+        SqlConnection sqlConnection = (SqlConnection)_db.Database.GetDbConnection();
+
+        var sql = "UPDATE Users SET username = @Username, name = @Name, email = @Email, password = @Password, createdDate = @CreatedDate, deletedDate = @DeletedDate WHERE userId = @Id";
+        var cmd = new SqlCommand(sql, sqlConnection);
+        cmd.Parameters.AddWithValue("@Id", userId);
+        cmd.Parameters.AddWithValue("@Username", updatedUser.Username);
+        cmd.Parameters.AddWithValue("@Name", updatedUser.Name);
+        cmd.Parameters.AddWithValue("@Email", updatedUser.Email);
+        cmd.Parameters.AddWithValue("@Password", updatedUser.Password);
+        cmd.Parameters.AddWithValue("@CreatedDate", updatedUser.CreatedDate ?? (object)DBNull.Value);
+        cmd.Parameters.AddWithValue("@DeletedDate", updatedUser.DeletedDate ?? (object)DBNull.Value);
+
+        sqlConnection.Open();
+        int rowsAffected = cmd.ExecuteNonQuery();
+        sqlConnection.Close();
+
+        if (rowsAffected > 0)
+        {
+            return Ok("User updated successfully.");
+        }
+        else
+        {
+            return NotFound("User not found.");
+        }
+    }
+
 
 }
--- server/Models/User.cs
+++ server/Models/User.cs
@@ -7,12 +7,13 @@ public class User
     [Key] /* annotation */
     public int UserId { get; set; }
 
+    [Required]
     public string Username { get; set; } = string.Empty;
 
-    [Required] /* annotation */
+    [Required]
     public string Name { get; set; } = string.Empty;
     [Display(Name = "The Name")]
-    [Range(1, 100, ErrorMessage = "This is a error message for the name")]
+    // [Range(1, 100, ErrorMessage = "This is a error message for the name")]
 
     [Required]
     public string Email { get; set; } = string.Empty;
@@ -20,8 +21,11 @@ public class User
     [Required]
     public string Password { get; set; } = string.Empty;
 
-    public DateTime CreatedDate { get; set; } = DateTime.Now;
-
-    public DateTime DeletedDate { get; set; } = DateTime.Now;
+    // public DateTime? CreatedDate { get; set; } = DateTime.Now;
+    // the default value above makes not allow to nulls.
+    public DateTime? CreatedDate { get; set; }
+    
+    // public DateTime? DeletedDate { get; set; } = DateTime.Now;
+    public DateTime? DeletedDate { get; set; }
 
 }
