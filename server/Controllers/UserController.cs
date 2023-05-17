751af7eec353d1b11e9d74aa238d4d1a460ece15
Suleyman <silo1881@gmail.com>
2023-05-17 17:29:09 +0300

    Remove forecast and getbyname

--- server/Controllers/UserController.cs
+++ server/Controllers/UserController.cs
@@ -30,7 +30,7 @@ public class UserController : ControllerBase
         }
     }
 
-    [HttpGet("UserInfoById/id")]
+    [HttpGet("id")]
     public IActionResult GetUserInfoById(int id)
     {   
         /*** returns all info ***/
@@ -73,70 +73,7 @@ public class UserController : ControllerBase
         return NotFound(new ApiResponse(false, "User request is unsuccessful since user couldn't be found", null));
     }
 
-    [HttpGet("UserOnlyClubsById/id")]
-    public List<Server.Models.Club> GetUserClubsById(int id)
-    {   
-        List<Club> clubs = new List<Club>();
-        using (SqlConnection connection = (SqlConnection)_db.Database.GetDbConnection())
-        {
-            string query = "SELECT c.* FROM Club c INNER JOIN ClubMembers cm ON c.clubId = cm.clubId INNER JOIN [Users] u ON u.userId = cm.userId WHERE u.userId = @userId";
-            SqlCommand command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@userId", id);
-
-            connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
-
-            while (reader.Read())
-            {
-                Club club = new Club
-                {
-                    clubId = reader.GetInt32(0),
-                    slug = reader.GetString(1),
-                    name = reader.GetString(2),
-                    description = reader.GetString(3),
-                    image = reader.GetString(4),
-                    validFrom = reader.IsDBNull(5) ? null : reader.GetDateTime(5),
-                    validUntil = reader.IsDBNull(6) ? null : reader.GetDateTime(6)
-                };
-                clubs.Add(club);
-            }
-            reader.Close();
-        }
-        return clubs;
-    }
-
-    [HttpGet("UserNoClubsById/id")]
-    public IActionResult GetUserNoClubsById(int id)
-    {   
-        /*** only returns username ***/
-        // // Get the underlying SqlConnection object
-        // SqlConnection sqlConnection = (SqlConnection)_db.Database.GetDbConnection();
-
-        // // get sql connection from appsettings.json
-        // var sql = "SELECT * FROM Users WHERE userId = @Id";
-        // var cmd = new SqlCommand(sql, sqlConnection);
-        // cmd.Parameters.AddWithValue("@Id", id);
-
-        // sqlConnection.Open();
-        // var reader = cmd.ExecuteReader();
-        // if (reader.Read())
-        // {
-        //     var name = reader.GetString(1);
-        //     return name;
-        // }
-
-        /*** returns all info ***/
-        User? user = _db.Users.SingleOrDefault(u => u.userId == id);
-
-        if (user != null)
-        {
-            return Ok(new ApiResponse(true, "User request is successfull", user));
-        }
-
-        return NotFound(new ApiResponse(false, "User request is unsuccessful since user couldn't be found", null));
-    }
-
-    [HttpGet("UserInfoById/username")]
+    [HttpGet("username")]
     public IActionResult GetUserByUsername(string username)
     {      
         /*** returns all info ***/
