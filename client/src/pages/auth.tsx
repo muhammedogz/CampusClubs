a2e456c0e6635ca58244be04e2c3eacc30c14775
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2023-06-10 02:48:20 +0300

    Handle user signup component

--- client/src/pages/auth.tsx
+++ client/src/pages/auth.tsx
@@ -1,10 +1,163 @@
-import { Backdrop, CircularProgress, Stack } from '@mui/material';
+import {
+  Backdrop,
+  Button,
+  Checkbox,
+  CircularProgress,
+  FormControlLabel,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { Box, Container } from '@mui/system';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import Image from 'src/components/common/Image';
+import { Layout } from 'src/components/layout/Layout';
 import { Endpoints, getApiEndpoint } from 'src/data/endpoints';
 import { ApiResponseType } from 'src/types/types';
+import { getLocalImage } from 'src/utils/imageUtils';
 import { StorageKeyEnum, getLocalStorageItem } from 'src/utils/storageUtils';
+
+type SignUpContentProps = {
+  auth: AuthResponse;
+};
+
+type UserSignUpType = {
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  file: File | null;
+};
+
+const SignUpContent = ({ auth }: SignUpContentProps) => {
+  const [user, setUser] = useState<UserSignUpType>({
+    username: auth.kullanici_adi,
+    email: auth.kurumsal_email_adresi,
+    firstName: '',
+    lastName: '',
+    file: null,
+  });
+
+  const handleSubmit = () => {
+    console.log({ user });
+  };
+
+  return (
+    <Layout>
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Image
+            src={getLocalImage('/icons/logo-white.svg')}
+            alt="logo"
+            width="250px"
+          />
+          <Typography component="h1" variant="h5">
+            Kayıt Ol
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="Kullanıcı Adı"
+                  label="Kullanıcı Adı"
+                  onChange={(e) => {
+                    setUser({ ...user, username: e.target.value });
+                  }}
+                  required
+                  disabled
+                  fullWidth
+                  value={auth.kullanici_adi}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="Email"
+                  label="Email"
+                  name="email"
+                  onChange={(e) => {
+                    setUser({ ...user, email: e.target.value });
+                  }}
+                  required
+                  disabled
+                  fullWidth
+                  value={auth.kurumsal_email_adresi}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="firstName"
+                  onChange={(e) => {
+                    setUser({ ...user, firstName: e.target.value });
+                  }}
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="İsim"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  id="lastName"
+                  label="Soyisim"
+                  name="lastName"
+                  onChange={(e) => {
+                    setUser({ ...user, lastName: e.target.value });
+                  }}
+                  required
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button variant="contained" component="label">
+                  Resim Yükle
+                  <input
+                    hidden
+                    type="file"
+                    name="file"
+                    onChange={(e) => {
+                      setUser({ ...user, file: e.target.files?.[0] ?? null });
+                    }}
+                  />
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              onClick={handleSubmit}
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </Layout>
+  );
+};
 
 type AuthResponse = {
   kullanici_adi: string;
@@ -51,8 +204,8 @@ const Auth = () => {
 
   if (auth?.auth_token) {
     return <Stack>Giriş Yapıldı</Stack>;
-  } else if (auth?.kullanici_adi && auth?.kurumsal_email_adresi) {
-    return <Stack>Üye ol modalı</Stack>;
+  } else if (auth) {
+    return <SignUpContent auth={auth} />;
   }
 
   return (
