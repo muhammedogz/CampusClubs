e425de81e2b32cbcabbb5f06c566432580d01c2b
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2023-06-17 23:33:00 +0300

    Add fetchr for joining club approval

--- client/src/fetch/clubFetchers.ts
+++ client/src/fetch/clubFetchers.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { Endpoints, getApiEndpoint } from 'src/data/endpoints';
-import { ApiResponseType, ClubType } from 'src/types/types';
+import { ApiResponseType, ApprovalStatusEnum, ClubType } from 'src/types/types';
 
 export const getAllClubsFetcher = async () => {
   const { data } = await axios.get<ApiResponseType<ClubType[]>>(
@@ -13,6 +13,27 @@ export const getAllClubsFetcher = async () => {
 export const getClubFromIdFetcher = async (id: string) => {
   const { data } = await axios.get<ApiResponseType<ClubType>>(
     `${getApiEndpoint(Endpoints.CLUBS)}/${id}`
+  );
+
+  return data;
+};
+
+type ApprovalJoinClubFuncType = {
+  clubId: string;
+  userId: string;
+  approveStatus: ApprovalStatusEnum;
+};
+
+export const approveCreatingEventFetcher = async ({
+  clubId,
+  userId,
+  approveStatus,
+}: ApprovalJoinClubFuncType) => {
+  const { data } = await axios.patch<ApiResponseType<ClubType>>(
+    `${getApiEndpoint(Endpoints.CLUBS)}/${clubId}/approval/${userId}`,
+    {
+      status: approveStatus,
+    }
   );
 
   return data;
