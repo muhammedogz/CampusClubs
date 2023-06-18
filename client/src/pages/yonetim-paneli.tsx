a0d12dbcae1a748f4756716ba589b70c3d1b32b3
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2023-06-18 22:40:02 +0300

    Add adding new user to club

--- client/src/pages/yonetim-paneli.tsx
+++ client/src/pages/yonetim-paneli.tsx
@@ -11,6 +11,7 @@ import { useNavigate } from 'react-router-dom';
 import CCButton from 'src/components/common/CCButton';
 import ImageNameStack from 'src/components/common/ImageNameStack';
 import { Layout } from 'src/components/layout/Layout';
+import AddUserClubModal from 'src/components/modals/AddUserClubModal';
 import ClubCreateModal from 'src/components/modals/ClubCreateModal';
 import ClubUpdateModal from 'src/components/modals/UpdateClubModal';
 import { Routes } from 'src/data/routes';
@@ -19,7 +20,7 @@ import { ClubType, UserRoleEnum } from 'src/types/types';
 import { StorageKeyEnum, getLocalStorageItem } from 'src/utils/storageUtils';
 import { generateRoute } from 'src/utils/urlUtils';
 
-const AllClubs = () => {
+const AllClubsEdit = () => {
   const [clubs, setClubs] = useState<ClubType[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [openUpdateClubDialog, setOpenUpdateClubDialog] = useState(false);
@@ -91,6 +92,78 @@ const AllClubs = () => {
   );
 };
 
+const AllClubsAddUser = () => {
+  const [clubs, setClubs] = useState<ClubType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [openUpdateClubDialog, setOpenUpdateClubDialog] = useState(false);
+  const [selectedClub, setSelectedClub] = useState<ClubType | null>(null);
+
+  const navigate = useNavigate();
+
+  const getAllClubs = useCallback(async () => {
+    try {
+      setLoading(true);
+      const clubsRespone = await getAllClubsFetcher();
+      if (clubsRespone.status) {
+        setClubs(clubsRespone.data);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getAllClubs();
+  }, [getAllClubs]);
+
+  return (
+    <Stack>
+      {selectedClub && (
+        <AddUserClubModal
+          onClose={() => {
+            navigate(0);
+          }}
+          open={openUpdateClubDialog}
+          clubId={selectedClub.clubId}
+        />
+      )}
+      {loading ? (
+        <Stack>
+          <CircularProgress
+            size={50}
+            sx={{
+              color: '#78ee11',
+            }}
+          />
+        </Stack>
+      ) : (
+        <Stack>
+          {clubs.map((club) => (
+            <Stack key={club.name + club.description}>
+              <ImageNameStack
+                data={{
+                  image: club.image,
+                  name: club.name,
+                  id: club.clubId,
+                }}
+              />
+              <CCButton
+                onClick={() => {
+                  setSelectedClub(club);
+                  setOpenUpdateClubDialog(true);
+                }}
+              >
+                Bu kulübe kullanıcı ekle
+              </CCButton>
+            </Stack>
+          ))}
+        </Stack>
+      )}
+    </Stack>
+  );
+};
+
 const Panel = () => {
   const [openCreateClubDialog, setOpenCreateClubDialog] = useState(false);
   const user = getLocalStorageItem(StorageKeyEnum.USER_STORAGE)?.user;
@@ -135,7 +208,20 @@ const Panel = () => {
               <Typography>Kulüpleri düzenle</Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <AllClubs />
+              <AllClubsEdit />
+            </AccordionDetails>
+          </Accordion>
+        </Stack>
+        <Stack>
+          <Accordion>
+            <AccordionSummary
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography>Kulüplere kullanıcı ekkle</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <AllClubsAddUser />
             </AccordionDetails>
           </Accordion>
         </Stack>
